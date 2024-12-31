e7e50537cc987f263b925269c19566aed807d171
???? <mhxy13867806343@users.noreply.github.com>
2024-12-31 19:34:50 +0800

    第一次提交，进行中。

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/vue-vite-windows-icon/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'vue-router', 'element-plus']
+        }
+      }
     }
   }
 })
