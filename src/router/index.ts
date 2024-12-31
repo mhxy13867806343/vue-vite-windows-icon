836a726e9a2e1c4b59e80a4bfccf6c49c4510685
???? <mhxy13867806343@users.noreply.github.com>
2024-12-31 20:21:09 +0800

    fix: update router to use hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,11 @@ import WindowsIcons from '../views/WindowsIcons.vue'
 import CharacterStatus from '../views/CharacterStatus.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
-      name: 'windows-icons',
+      name: 'home',
       component: WindowsIcons
     },
     {
